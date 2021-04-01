b9b5cfa1a9c594f399731d954826439b18801aa5
Keyu Liu <keyuliu@KeyudeMacBook-Pro.local>
2021-04-01 10:48:45 -0400

    add load balancernew conf in Route53  and related IAM policy and roles

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 
 resource "aws_vpc" "vpc-1" {
-    cidr_block              = "10.1.0.0/16"
+    cidr_block              = "10.0.0.0/16"
     enable_dns_hostnames    = true
     enable_dns_support      = true
     assign_generated_ipv6_cidr_block = false
@@ -26,7 +26,7 @@ resource "aws_vpc" "vpc-3" {
 
 
 resource "aws_subnet" "subnet-1" {
-    cidr_block              = "10.1.1.0/24"
+    cidr_block              = "10.0.1.0/24"
     vpc_id                  = aws_vpc.vpc-1.id
     availability_zone       = var.az1
     map_public_ip_on_launch = true
@@ -36,7 +36,7 @@ resource "aws_subnet" "subnet-1" {
 }
 
 resource "aws_subnet" "subnet-2" {
-    cidr_block              = "10.1.2.0/24"
+    cidr_block              = "10.0.2.0/24"
     vpc_id                  = aws_vpc.vpc-1.id
     availability_zone       = var.az2
     map_public_ip_on_launch = true
@@ -46,7 +46,7 @@ resource "aws_subnet" "subnet-2" {
 }
 
 resource "aws_subnet" "subnet-3" {
-    cidr_block              = "10.1.3.0/24"
+    cidr_block              = "10.0.3.0/24"
     vpc_id                  = aws_vpc.vpc-1.id
     availability_zone       = var.az3
     map_public_ip_on_launch = true
@@ -231,7 +231,8 @@ resource "aws_db_instance" "webappdb" {
   final_snapshot_identifier = "webappdbsnapshot"
   allocated_storage = 20
 }
-
+/* WebAppSecurityGroup is for auto-scaling, while application is for ec2
+we are using auto-scaling instead of ec2, so comment here
 resource "aws_instance" "webapp" {
 
   ami           = var.amiID
@@ -263,6 +264,7 @@ resource "aws_instance" "webapp" {
   }
 
 }
+*/
 
 resource "aws_iam_policy" "WebAppS3" {
   name = "WebAppS3"
@@ -542,11 +544,251 @@ resource "aws_iam_role" "CodeDeployServiceRole" {
   EOF
 }
 
+
+
+
+# -------------------------------------------------------------------
+# load balancer aws_security_group
+resource "aws_security_group" "load_balancer" {
+  name        = "security-group-lb"
+  description = "only allow 80 for ingress"
+  vpc_id      = aws_vpc.vpc-1.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [var.security_group_cidr_block]
+  }
+}
+
+resource "aws_security_group_rule" "lb_sgr" {
+  count             = length(var.aws_security_group_lb_ingress_port)
+  type              = var.security_group_rule_in
+  from_port         = element(var.aws_security_group_lb_ingress_port,count.index)
+  to_port           = element(var.aws_security_group_lb_ingress_port,count.index)
+  protocol          = var.security_group_protocl_in
+  cidr_blocks       = [var.security_group_cidr_block]
+  security_group_id = aws_security_group.load_balancer.id
+}
+
+# -------------------------------------------------------------------
+# Autoscaling Launch Configuration Security Group: WebAppSecurityGroup
+resource "aws_security_group" "autoscale_launch_config" {
+  name        = var.aws_autoscale_launch_config_security_group
+  description = var.aws_security_group_app_desc
+  vpc_id      = aws_vpc.vpc-1.id
+
+  egress {
+    from_port   = var.all_port
+    to_port     = var.all_port
+    protocol    = var.security_group_protocl_e
+    cidr_blocks = [var.security_group_cidr_block]
+  }
+
+  tags = {
+    Name = var.aws_autoscale_launch_config_security_group
+  }
+}
+
+resource "aws_security_group_rule" "autoscale_launch_config_sgr" {
+  count             = length(var.aws_security_group_ingress_port)
+  type              = var.security_group_rule_in
+  from_port         = element(var.aws_security_group_ingress_port,count.index)
+  to_port           = element(var.aws_security_group_ingress_port,count.index)
+  protocol          = var.security_group_protocl_in
+  #cidr_blocks       = [var.security_group_cidr_block]
+  security_group_id = aws_security_group.autoscale_launch_config.id
+  #security_groups   = [aws_security_group.load_balancer.id]
+  source_security_group_id  = aws_security_group.load_balancer.id
+}
+
+/*resource "aws_subnet" "subnet123" {
+  count = length(var.subnet_cidr_block)
+  cidr_block           = element(var.subnet_cidr_block,count.index)
+  vpc_id = aws_vpc.vpc-1.id
+  availability_zone    = element(var.azs,count.index)
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "subnet-${var.vers}-${count.index}"
+  }
+}*/
+
+# -------------------------------------------------------------------
+# Application Load Balancer
+resource "aws_lb" "app_lb" {
+  name               = var.app_load_balancer_name
+  internal           = var.fbool
+  load_balancer_type = var.app_load_balancer_type
+  security_groups    = [aws_security_group.load_balancer.id]
+  subnets            = [aws_subnet.subnet-1.id,aws_subnet.subnet-2.id]
+}
+
+# -------------------------------------------------------------------
+# target group
+resource "aws_lb_target_group" "lb_target_group" {
+  name     = var.lb_target_group_name
+  port     = var.lb_target_group_port
+  protocol = var.app_load_balancer_protocol
+  vpc_id   = aws_vpc.vpc-1.id
+  health_check{
+    path     = "/v1/users"
+    port     = 8080
+    interval = 300
+  }
+}
+
+# Application Load Balancer listener: http-80
+resource "aws_lb_listener" "app_lb_listener" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = var.app_lb_listener_port
+  protocol          = var.app_load_balancer_protocol
+  default_action {
+    type             = var.app_load_balancer_action_type
+    target_group_arn = aws_lb_target_group.lb_target_group.arn
+  }
+}
+
+# Application Load Balancer listener: https-443
+/*resource "aws_lb_listener" "app_lb_listener_https" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  certificate_arn   = data.aws_acm_certificate.issued.arn
+
+  default_action {
+    type             = var.app_load_balancer_action_type
+    target_group_arn = aws_lb_target_group.lb_target_group.arn
+  }
+}*/
+# -------------------------------------------------------------------
+# Autoscaling Launch Configuration for EC2 Instances
+resource "aws_launch_configuration" "aws_conf" {
+  name          = var.aws_launch_configuration_name
+  image_id      = var.amiID
+  instance_type = "t2.micro"
+  key_name                    = "CSYE"
+  associate_public_ip_address = var.tbool
+  /*
+  user_data                   = <<EOF
+#!/bin/bash
+echo DB_USERNAME="${var.rds_username}" >> /etc/environment
+echo DB_PASSWORD="${var.password}" >> /etc/environment
+echo DB_NAME="${var.aws_dynamodb_table_name}" >> /etc/environment
+echo DBHOSTNAME="${aws_db_instance.db.endpoint}" >> /etc/environment
+echo BUCKET_NAME="${var.aws_s3_bucket_name}" >> /etc/environment
+  EOF
+  */
+  iam_instance_profile        = aws_iam_instance_profile.profile1.name
+  security_groups             = [aws_security_group.autoscale_launch_config.id]
+
+  root_block_device {
+    volume_type            = "gp2"
+    volume_size            = 20
+    delete_on_termination  = true
+  }
+}
+
+# -------------------------------------------------------------------
+# Autoscaling group
+resource "aws_autoscaling_group" "aws_autoscale_gr" {
+  name                      = var.aws_autoscaling_group_name
+  default_cooldown          = 60
+  launch_configuration      = aws_launch_configuration.aws_conf.name
+  health_check_grace_period = 300
+  health_check_type         = "EC2"
+  max_size                  = 5
+  min_size                  = 3
+  desired_capacity          = 3
+  vpc_zone_identifier       = aws_subnet.subnet-1.*.id
+
+  tag {
+    key                     = var.aws_autoscaling_group_tag_key
+    value                   = var.aws_autoscaling_group_tag_value
+    propagate_at_launch     = var.tbool
+  }
+
+  target_group_arns         = [aws_lb_target_group.lb_target_group.arn]
+}
+
+# -------------------------------------------------------------------
+# Autoscaling group scale up policy
+resource "aws_autoscaling_policy" "autoscaling_scale_up_policy" {
+  name                   = var.aws_autoscaling_scale_up_policy_name
+  scaling_adjustment     = var.aws_autoscaling_scale_up_policy_scaling_adjustment
+  adjustment_type        = var.aws_autoscaling_scale_up_policy_adjustment_type
+  cooldown               = var.aws_autoscaling_scale_up_policy_cooldown
+  autoscaling_group_name = aws_autoscaling_group.aws_autoscale_gr.name
+}
+
+# -------------------------------------------------------------------
+# cloud watch alarm for Autoscaling group scale up policy
+resource "aws_cloudwatch_metric_alarm" "cloudwatch_scale_up_alarm" {
+  alarm_name          = var.cloudwatch_scale_up_alarm_name
+  alarm_description   = var.cloudwatch_scale_up_alarm_description
+  metric_name         = var.cloudwatch_scale_up_alarm_metric_name
+  namespace           = var.cloudwatch_scale_up_alarm_namespace
+  statistic           = var.cloudwatch_scale_up_alarm_statistic
+  period              = var.cloudwatch_scale_up_alarm_period
+  evaluation_periods  = "10"
+  threshold           = var.cloudwatch_scale_up_alarm_threshold
+  alarm_actions       = [aws_autoscaling_policy.autoscaling_scale_up_policy.arn]
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.aws_autoscale_gr.name
+  }
+  comparison_operator = var.cloudwatch_scale_up_alarm_comparison_operator
+}
+
+# -------------------------------------------------------------------
+# Autoscaling group scale down policy
+resource "aws_autoscaling_policy" "autoscaling_scale_down_policy" {
+  name                   = var.aws_autoscaling_scale_down_policy_name
+  scaling_adjustment     = var.aws_autoscaling_scale_down_policy_scaling_adjustment
+  adjustment_type        = var.aws_autoscaling_scale_up_policy_adjustment_type
+  cooldown               = var.aws_autoscaling_scale_up_policy_cooldown
+  autoscaling_group_name = aws_autoscaling_group.aws_autoscale_gr.name
+}
+
+# -------------------------------------------------------------------
+# cloud watch alarm for Autoscaling group scale down policy
+resource "aws_cloudwatch_metric_alarm" "cloudwatch_scale_down_alarm" {
+  alarm_name          = var.cloudwatch_scale_down_alarm_name
+  alarm_description   = var.cloudwatch_scale_down_alarm_description
+  metric_name         = var.cloudwatch_scale_up_alarm_metric_name
+  namespace           = var.cloudwatch_scale_up_alarm_namespace
+  statistic           = var.cloudwatch_scale_up_alarm_statistic
+  period              = var.cloudwatch_scale_up_alarm_period
+  evaluation_periods  = "10"
+  threshold           = var.cloudwatch_scale_down_alarm_threshold
+  alarm_actions       = [aws_autoscaling_policy.autoscaling_scale_down_policy.arn]
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.aws_autoscale_gr.name
+  }
+  comparison_operator = var.cloudwatch_scale_down_alarm_comparison_operator
+}
+
+# -------------------------------------------------------------------
+# DNS record of ec2 public ip
 data "aws_route53_zone" "webapp_route53_hosted_zone"{
   name          = var.domain_name
   private_zone = false
 }
 
+resource "aws_route53_record" "lb_alias_record" {
+  zone_id = data.aws_route53_zone.webapp_route53_hosted_zone.zone_id
+  name    = format("%s.6225csyekeyuliu.me.", var.env)
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.app_lb.dns_name
+    zone_id                = aws_lb.app_lb.zone_id
+    evaluate_target_health = var.fbool
+  }
+}
+
+/*
 resource "aws_route53_record" "www" {
   zone_id = data.aws_route53_zone.webapp_route53_hosted_zone.zone_id
   name    = var.domain_name
@@ -554,4 +796,45 @@ resource "aws_route53_record" "www" {
   ttl     = "60"
   records = [aws_instance.webapp.public_ip]
 }
+*/
 
+/*
+# -------------------------------------------------------------------
+# ssh key pair
+resource "aws_key_pair" "ssh" {
+  key_name   = var.aws_key_pair_name
+  public_key = var.aws_key_pair_key
+}
+*/
+
+/*# -------------------------------------------------------------------
+# Create CodeDeploy Application
+resource "aws_codedeploy_app" "codedeploy_app" {
+  compute_platform = var.codedeploy_app_cp
+  name             = var.codedeploy_app_name
+}
+# -------------------------------------------------------------------
+# Create CodeDeploy Deployment Group
+resource "aws_codedeploy_deployment_group" "codedeploy_deployment_group" {
+  app_name              = aws_codedeploy_app.codedeploy_app.name
+  deployment_group_name = var.codedeploy_deployment_group_name
+  service_role_arn      = aws_iam_role.code_deploy_service_role.arn
+  deployment_config_name = var.codedeploy_deployment_group_deployment_config_name
+
+  deployment_style {
+    deployment_type = var.codedeploy_deployment_group_deployment_style
+  }
+
+  ec2_tag_set {
+    ec2_tag_filter {
+      key   = var.codedeploy_deployment_group_ec2_tag_filter_key
+      type  = var.codedeploy_deployment_group_ec2_tag_filter_type
+      value = var.codedeploy_deployment_group_ec2_tag_filter_value
+    }
+  }
+
+  auto_rollback_configuration {
+    enabled = var.fbool
+    # events  = ["DEPLOYMENT_FAILURE"]
+  }
+}*/
